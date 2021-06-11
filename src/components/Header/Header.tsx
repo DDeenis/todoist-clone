35d5592b58b5bf40556f2395503a30974f5c4ef2
DDeenis <ddeenis46@gmail.com>
2021-06-11 16:48:27 +0300

    header finished

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { FaPlus, FaSun, FaMoon } from "react-icons/fa";
+import logo from '../../assets/images/logo.png';
+import './Header.scss';
+
+const Header = (): JSX.Element => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    return (
+        <header className='header'>
+            <div className='header-content-wrapper'>
+                <div className='logo'>
+                    <img src={logo} className='logo__img' alt='Todoist logo' />
+                </div>
+                <div className='header-nav'>
+                    <ul className='header-nav__ul'>
+                        <li className='header-nav__li'>
+                            <button className='header-nav__button'>
+                                <FaPlus className='header-nav__icon' />
+                            </button>
+                        </li>
+                        <li className='header-nav__li'>
+                            <button className='header-nav__button' onClick={() => setIsDarkMode(!isDarkMode)}>
+                                {
+                                    isDarkMode
+                                        ? <FaSun className='header-nav__icon' />
+                                        : <FaMoon className='header-nav__icon' />
+                                }
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
