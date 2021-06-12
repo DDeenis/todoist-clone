302fd760dbc8b8342c35f0f97e809d68a8e5fe24
DDeenis <ddeenis46@gmail.com>
2021-06-12 21:35:58 +0300

    fixes and little improvements

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { FaInbox, FaCalendar, FaCalendarWeek, FaChevronDown } from "react-icons/fa";
+import { useState } from 'react';
+import { FaInbox, FaCalendar, FaCalendarWeek, FaChevronDown, FaChevronRight } from "react-icons/fa";
 import './Sidebar.scss';
 import SidebarEntry from './SidebarEntry/SidebarEntry';
 
 const Sidebar = (): JSX.Element => {
+    const [isToggled, setIsToggled] = useState(false);
+
     return (
         <aside className='main-sidebar'>
             <div className='sidebar-content-wrapper'>
                 <ul className='sibedar-content'>
-                    <SidebarEntry title='Inbox' icon={<FaInbox className='sidebar-nav-entry__icon' />} />
-                    <SidebarEntry title='Today' icon={<FaCalendar className='sidebar-nav-entry__icon' />} />
-                    <SidebarEntry title='Next 7 days' icon={<FaCalendarWeek className='sidebar-nav-entry__icon' />} />
-                    <li className='sidebar-content__li sidebar-projects-burger'>
+                    <SidebarEntry title='Inbox' link='inbox' icon={<FaInbox className='sidebar-nav-entry__icon' />} />
+                    <SidebarEntry title='Today' link='today' icon={<FaCalendar className='sidebar-nav-entry__icon' />} />
+                    <SidebarEntry title='Next 7 days' link='week' icon={<FaCalendarWeek className='sidebar-nav-entry__icon' />} />
+                    <li className='sidebar-content__li sidebar-projects-burger' onClick={() => setIsToggled(!isToggled)}>
                         <div className='sidebar-nav-entry'>
-                            <FaChevronDown className='sidebar-nav-entry__icon' />
+                            {
+                                isToggled
+                                    ? <FaChevronDown className='sidebar-nav-entry__icon' />
+                                    : <FaChevronRight className='sidebar-nav-entry__icon' />
+                            }
                             <span className='sidebar-nav-entry__span sidebar-nav-entry__span_active'>Projects</span>
                         </div>
                     </li>
--- src/components/Sidebar/SidebarEntry/SidebarEntry.tsx
+++ src/components/Sidebar/SidebarEntry/SidebarEntry.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './SidebarEntry.scss';
+import { Link } from 'react-router-dom';
 
 interface SidebarEntryProps {
     title: string,
+    link: string,
     icon: JSX.Element,
     active?: boolean,
     onClick?: React.MouseEventHandler<HTMLLIElement>
 }
 
-const SidebarEntry = ({ title, icon, active = false, onClick = (e) => e.stopPropagation() }: SidebarEntryProps): JSX.Element => {
+const SidebarEntry = ({ title, link, icon, active = false, onClick = (e) => e.stopPropagation() }: SidebarEntryProps): JSX.Element => {
     return (
         <li className='sidebar-content__li' onClick={onClick}>
             <div className='sidebar-nav-entry'>
                 { icon }
-                <span className={
-                    active
-                        ? 'sidebar-nav-entry__span sidebar-nav-entry__span_active'
-                        : 'sidebar-nav-entry__span'
-                }>{title}</span>
+                <Link to={link}>
+                    <span className={
+                        active
+                            ? 'sidebar-nav-entry__span sidebar-nav-entry__span_active'
+                            : 'sidebar-nav-entry__span'
+                    }>{title}</span>
+                </Link>
             </div>
         </li>
     );
